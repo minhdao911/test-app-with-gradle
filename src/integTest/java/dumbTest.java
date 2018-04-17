1c3c03a27f11e1a41fcccd98214c876b208a2df9
Minh Dao <daominh91197@gmail.com>
2018-04-17 21:33:36 +0300

    set integration test

---
+++ src/integTest/java/dumbTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author minhdao
+ */
+public class dumbTest {
+    @Test
+    public void dummyIntegrationTest(){
+        assertEquals("hello", "hi");
+    }
+    @Test
+    public void dummyIntegrationTest2(){
+        assertEquals(10, 10);
+    }
+}
