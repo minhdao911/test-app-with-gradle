80294410fabd5df14452e22f2ee5ff8b09ca6016
Minh Dao <daominh91197@gmail.com>
2018-04-17 11:04:49 +0300

    add unit test

---
+++ src/test/java/org/gradle/demo/HelloServletTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.gradle.demo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author minhdao
+ */
+public class HelloServletTest {
+    @Test
+    public void dummyUnitTest(){
+        assertEquals(1, 1);
+    }
+    @Test
+    public void dummyUnitTest2(){
+        assertEquals(0, 1);
+    }
+}
